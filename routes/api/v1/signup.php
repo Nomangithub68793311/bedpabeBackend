791b3bde13d080f63e8b1a934211a3267d7b850b
codecommit <rana.buddy@gmail.com>
2022-10-25 19:57:52 +0600

    updated database

--- routes/api/v1/signup.php
+++ routes/api/v1/signup.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
 
-Route::post('/user/signup', [SignupController::class,'store'])->middleware('IpCheckAndAllow');
-Route::post('/user/login', [SignupController::class,'login'])->middleware('IpCheckAndAllow');
+Route::post('/user/signup', [AccountController::class,'store'])->middleware('IpCheckAndAllow');
+Route::post('/user/login', [AccountController::class,'login'])->middleware('IpCheckAndAllow');
 
